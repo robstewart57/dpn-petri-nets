cbfe6b302d12faf6c175bc00ff3edc7d5ed3d629
Rob Stewart <robstewart57@gmail.com>
2017-01-17 18:05:27 +0000

    Changes to dot syntax output

--- src/ActorToDot.hs
+++ src/ActorToDot.hs
@@ -9,6 +9,7 @@ import AbsCAL
 import PrintCAL
 
 import Data.List
+import Data.Maybe
 
 parseCal :: String -> Actor
 parseCal s =
@@ -44,20 +45,132 @@ ports (ActrNoPkg _ _ _ (IOSg ins outs) _ _ _ _) =
     showPorts (PortDcl _ (Ident ident):xs) =
       (ident ++ "[style=dashed]") : showPorts xs
 
-transitions :: Actor -> [String]
-transitions (ActrNoPkg _ _ _ _ _ blocks _ _) =
-  map showActionCode blocks
+-- s0->act1 [label=" st"]
+-- act1->s1 [label="st#bl = not st#bl"]
+fsmTransitions :: Actor -> [String]
+fsmTransitions (ActrNoPkg _ _ _ _ globVars blocks (SchedfsmFSM initState trans) _) =
+  map (\(StTrans (Ident from) (Ident actionName) (Ident to)) ->
+         from
+         ++ "->"
+         ++ actionName
+         ++ " [label=\""
+         ++ (if fst (lookupStmts actionName) then "{}" else "st")
+         ++ "\"]"
+         ++ "\n"
+         ++ actionName
+         ++ "->"
+         ++ to
+         ++ " [label=\""
+         ++ snd (lookupStmts actionName)
+         ++ "\"]"
+         ) trans
+
   where
-    showActionCode (ActionCode (AnActn actn)) = showAction actn
-    showAction (ActnTagStmts tag actnHead stmts) =
-  
+    lookupStmts :: String -> (Bool,String)
+    lookupStmts actionName =
+      let allActions =
+            concatMap (\block ->
+                         case block of
+                           UnitCode _ -> []
+                           ActionCode (InitActn _) -> []
+                           ActionCode (AnActn action) -> [action]
+                      ) blocks
+          theAction = head (filter isThisAction allActions)
+          isThisAction (ActnTags (ActnTagDecl [Ident tag]) _) = tag == actionName
+          isThisAction (ActnTagsStmts (ActnTagDecl [Ident tag]) _ _) = tag == actionName
+          isThisAction _ = False
+
+          showStmts (ActnTags _ _) = ""
+          showStmts (ActnTagsStmts _ _ stmts) =
+            concat $ intersperse "," (map printTree (globalAssignStmts stmts))
+
+          globalAssignStmts [] = []
+          globalAssignStmts [s@(SemiColonSeparatedStmt (AssignStt st))]
+            | isGlobalAssign st = [s]
+            | otherwise = []
+          -- globalAssignStmts [_] = []
+          -- globalAssignStmts (SemiColonSeparatedStmt st@AssignStt{}:xs) =
+          --   st : globalAssignStmts xs
+          -- globalAssignStmts (_:xs) = globalAssignStmts xs
+
+          isGlobalAssign (AssStmt (Ident ident) _) = elem ident globalMutableVars
+          isGlobalAssign (AssStmtIdx (Ident ident) idx _) = elem ident globalMutableVars
+
+          globalMutableVars =
+            catMaybes
+            $ map (\(GlobVarDecl v) -> varName v) globVars
+          varName (VDecl _ (Ident v) _) = Just v
+          varName (VDeclExpMut _ (Ident v) _ _) = Just v
+          varName (VDeclExpIMut _ _ _ _) = Nothing
+
+          -- not really, can have statements that write only to local action vars
+          isActionPure (ActnTags _ _) = True
+          isActionPure _ = False
+
+      in (isActionPure theAction, showStmts theAction)
+
+-- In1->act1 [label="x"]
+-- act1->Out1 [label="x+1"]
+actionsPortIO :: Actor -> [String]
+actionsPortIO (ActrNoPkg _ _ _ _ _ blocks _ _) =
+  concatMap (\block ->
+         case block of
+           ActionCode (AnActn action) ->
+             case action of
+               ActnTags (ActnTagDecl [Ident tag]) actionHead ->
+                 showActionHeadPorts tag actionHead
+               ActnTagsStmts (ActnTagDecl [Ident tag]) actionHead _ ->
+                 showActionHeadPorts tag actionHead
+           _ -> [""]) blocks
+  where
+    showActionHeadPorts tag actionHead =
+                 case actionHead of
+                   ActnHead inPattern outPattern ->
+                     map (showInputPattern tag) inPattern
+                     ++ map (showOutputPattern tag) outPattern
+                   ActnHeadGuarded inPattern outPattern _ ->
+                     map (showInputPattern tag) inPattern
+                     ++ map (showOutputPattern tag) outPattern
+
+showInputPattern :: String -> InputPattern -> String
+showInputPattern actionName (InPattTagIds (Ident portName) tokens) =
+  portName ++ "->" ++ actionName {- ++ " [label=\"" ++ concatMap (\(Ident t) -> t) tokens ++ "\"]" -}
+
+showOutputPattern :: String -> OutputExp -> String
+showOutputPattern actionName (OutPattTagIds (Ident portName) _) =
+  actionName ++ "->" ++ portName
+
+-- act1 [shape=box,xlabel="[not st#bl]"]
+actionNodes :: Actor -> [String]
+actionNodes (ActrNoPkg _ _ _ _ _ blocks _ _) =
+  map (\block ->
+         case block of
+           ActionCode (AnActn action) ->
+             case action of
+               ActnTags (ActnTagDecl [Ident tag]) actionHead ->
+                 showActionHeadGuards tag actionHead
+               ActnTagsStmts (ActnTagDecl [Ident tag]) actionHead _ ->
+                 showActionHeadGuards tag actionHead
+           _ -> "") blocks
+  where
+    showActionHeadGuards tag actionHead =
+      case actionHead of
+        ActnHead _ _ -> tag ++ " [shape=box]"
+        ActnHeadGuarded _ _ exps ->
+          tag
+          ++ " [shape=box,xlabel=\"[" ++ concatMap printTree exps ++ "]\"]"
+      
+
 digraphDot :: Actor -> String
 digraphDot actor =
   unlines
   [ "digraph {"
+  , "rankdir=LR"
   , unlines (fsmStates actor)
   , unlines (ports actor)
-  , unlines (transitions actor)
+  , unlines (fsmTransitions actor)
+  , unlines (actionsPortIO actor)
+  , unlines (actionNodes actor)
   , "}"
   ]
 
@@ -69,12 +182,7 @@ digraphDot actor =
   -- Out1 [style=dashed]
   -- Out2 [style=dashed]
 
-  -- s0->act1 [label=" st"]
-  -- In1->act1 [label="x"]
-  -- act1->s1 [label="st#bl = not st#bl"]
-  -- act1->Out1 [label="x+1"]
-  -- act1 [shape=box,xlabel="[not st#bl]"]
-  
+
   -- s1->act2 [label=" st"]
   -- act2->s0 [label=" st#bl = not st#bl"]
   -- act2->Out2 [label="x-2"]
